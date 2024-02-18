33a34a874b6105f487b4f6614386b4e55623c4ae
Anjinhyoung <117788976+Anjinhyoung@users.noreply.github.com>
2024-02-18 17:46:19 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 128 ms, Memory: 110948 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,19 +1,15 @@
 a,b = map(int,input().split())
+c, d = [],[]
 
-l = [ [0] * b for _ in range(a) ]
-l2 = [ [0] * b for _ in range(a) ]
+for _ in range(a):
+    a2 = list(map(int,input().split()))
+    c.append(a2)
 
-for i in range(a):
-    l[i] = list(map(int, input().split()[:b]))
-
-for i in range(a):
-    l2[i] = list(map(int, input().split()[:b]))
-
-l3 = [ [0] * b for _ in range(a) ]
+for _ in range(a):
+    a2 = list(map(int,input().split()))
+    d.append(a2)
 
 for i in range(a):
     for j in range(b):
-        l3[i][j] = l2[i][j] + l[i][j]
-
-for i in l3:
-    print(*i)
+        print(c[i][j] + d[i][j], end=" ")
+    print()
